f2f5c0991d7bceea28adff7b9f9fef0d697eac56
CustomNick <55017726+CustomNick@users.noreply.github.com>
2021-12-04 13:54:48 +0300

    Static Function class is added along with method for reading file

---
+++ src/ChauvenetCriterionLib/Functions/Functions.cs
@@ -0,0 +1,11 @@
+﻿namespace Functions
+{
+    public static class Functions
+    {
+        public static string[] ReadTest(string path)
+        {
+            string[] lines = System.IO.File.ReadAllLines(path);
+            return lines;
+        }
+    }
+}
