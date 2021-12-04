00ea88a50ed89bfaf471c04357b4504c0a69bb03
IvanKalimanov <ikalimanov@mail.ru>
2021-12-05 00:25:32 +0300

    add elements to MainForm and basic logic

--- src/ChauvenetCriterionPresenter/Forms/MainForm.Designer.cs
+++ src/ChauvenetCriterionPresenter/Forms/MainForm.Designer.cs
@@ -29,13 +29,111 @@ namespace ChauvenetCriterionPresenter
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.InitialSampleBox = new System.Windows.Forms.ListBox();
+            this.ProcessedSampleBox = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(33, 462);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(200, 72);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Открыть";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // InitialSampleBox
+            // 
+            this.InitialSampleBox.FormattingEnabled = true;
+            this.InitialSampleBox.ItemHeight = 25;
+            this.InitialSampleBox.Location = new System.Drawing.Point(33, 17);
+            this.InitialSampleBox.Name = "InitialSampleBox";
+            this.InitialSampleBox.Size = new System.Drawing.Size(200, 379);
+            this.InitialSampleBox.TabIndex = 1;
+            // 
+            // ProcessedSampleBox
+            // 
+            this.ProcessedSampleBox.FormattingEnabled = true;
+            this.ProcessedSampleBox.ItemHeight = 25;
+            this.ProcessedSampleBox.Location = new System.Drawing.Point(558, 17);
+            this.ProcessedSampleBox.Name = "ProcessedSampleBox";
+            this.ProcessedSampleBox.Size = new System.Drawing.Size(200, 379);
+            this.ProcessedSampleBox.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(558, 462);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(200, 72);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Исключить выбросы";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.ImageLocation = "";
+            this.pictureBox1.Location = new System.Drawing.Point(305, 209);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(188, 70);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(286, 181);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(218, 25);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Chauvenet\'s Criterion";
+            this.label1.Visible = false;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 577);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.ProcessedSampleBox);
+            this.Controls.Add(this.InitialSampleBox);
+            this.Controls.Add(this.button1);
+            this.Name = "MainForm";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox InitialSampleBox;
+        private System.Windows.Forms.ListBox ProcessedSampleBox;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- src/ChauvenetCriterionPresenter/Forms/MainForm.cs
+++ src/ChauvenetCriterionPresenter/Forms/MainForm.cs
@@ -16,5 +16,43 @@ namespace ChauvenetCriterionPresenter
         {
             InitializeComponent();
         }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel)
+                return;
+            // получаем выбранный файл
+            string filename = openFileDialog1.FileName;
+            // читаем файл в строку
+            string[] lines = System.IO.File.ReadAllLines(filename);
+            InitialSampleBox.Items.AddRange(lines);
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (InitialSampleBox.Items.Count == 0)
+            {
+                MessageBox.Show("Choose file with initial sample");
+                return;
+            }
+            pictureBox1.Image = Image.FromFile("../../images/arrow_right.png");
+            pictureBox1.Visible = true;
+            label1.Visible = true;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
